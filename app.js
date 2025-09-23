12b4774de4a1fa117042a78a5dee66539ee627f9
Fabián Hernández C <carlosfabianh78@gmail.com>
2025-09-23 17:13:16 -0600

    Actulizado app.js y Agrego Readme

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let amigos = [];
 
-input = document.getElementById("amigo");
+let input = document.getElementById("amigo");
 
 function agregarAmigo() {
   const nombre = input.value.trim();
@@ -10,20 +10,37 @@ function agregarAmigo() {
     amigos.push(nombre);
     console.log(amigos);
     limpiarNombre();
+    mostrarAmigos();
   } else {
-    console.log("nombre no valido");
+    alert("Debes ingresar un nombre")
+    //console.log("nombre no valido");
   }
 }
 
 function sortearAmigo() {
-  if (input === "") {
-    console.log("error");
+  if (amigos === 0) {
+    console.log("No hay amigos");
   } else {
-    console.log(amigo.value);
+    let indice = Math.floor(Math.random() * amigos.length);
+    let nombreElegido = amigos[indice];
+
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `<li>${nombreElegido}</li>`;
+
+    //console.log(`Amigos sorteado es ${nombreElegido}`);
   }
 }
 
+function limpiarNombre() {
+  document.getElementById("amigo").value = "";
+}
 
-function limpiarNombre(){
-    document.getElementById("amigo").value = "";
+function mostrarAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (var i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
 }
